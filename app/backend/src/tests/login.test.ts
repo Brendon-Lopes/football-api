ab42b1dd32ff921b660e87bc418771947898adf5
Brendon <brendon.lopes.21@gmail.com>
2022-08-18 19:57:45 -0300

    test: :white_check_mark: adds tests to route /login/validate/

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -5,10 +5,11 @@ import chaiHttp = require('chai-http');
 import User from '../database/models/user';
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+// import Example from '../database/models/ExampleModel';
 
-import { Response } from 'superagent';
+// import { Response } from 'superagent';
 import ILoginDTO from '../interfaces/ILoginDTO.interface';
+import JWT from '../helpers/jwt';
 
 chai.use(chaiHttp);
 
@@ -24,17 +25,18 @@ const invalidUserMock: ILoginDTO = {
   password: "654321",
 }
 
-describe('User', () => {
-  describe('Login', () => {
+describe('POST /login', () => {
+  describe('when it succeeds', () => {
     beforeEach(() => {
       sinon.stub(User, 'findOne').resolves(userMock as User);
+      sinon.stub(JWT, 'sign').returns('validToken');
     });
 
     afterEach(() => {
       sinon.restore();
     });
 
-    it('when it succeeds should return status code 200', async () => {
+    it('should return status code 200', async () => {
       const response = await chai
         .request(app)
         .post('/login')
@@ -43,42 +45,32 @@ describe('User', () => {
       expect(response.status).to.equal(200);
     });
 
-    it('when it fails should return status code 400', async () => {
+    it('should return a token', async () => {
+      const response = await chai
+        .request(app)
+        .post('/login')
+        .send(userMock);
+      
+      expect(response.body).to.deep.equal({ token: 'validToken' });
+    });
+  });
+
+  describe('when it fails', () => {
+    beforeEach(() => {
+      sinon.stub(User, 'findOne').resolves(null);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    })
+
+    it('should return status code 401', async () => {
       const response = await chai
         .request(app)
         .post('/login')
         .send(invalidUserMock);
 
-      expect(response.status).to.equal(400);
+      expect(response.status).to.equal(401);
     });
   });
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
-
-  // let chaiHttpResponse: Response;
-
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  // it('Seu sub-teste', () => {
-  //   expect(false).to.be.eq(true);
-  // });
 });
---
+++ app/backend/src/tests/loginValidate.test.ts
@@ -0,0 +1,92 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import User from '../database/models/user';
+
+import { app } from '../app';
+
+import ILoginDTO from '../interfaces/ILoginDTO.interface';
+import JWT from '../helpers/jwt';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const userMock = {
+  email: "email@email.com",
+  role: "admin",
+}
+
+describe('GET /login/validate', () => {
+  describe('when it succeeds', () => {
+    beforeEach(() => {
+      sinon.stub(JWT, 'decode').returns({ email: userMock.email, password: '' });
+      sinon.stub(User, 'findOne').resolves(userMock as User);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should return status code 200', async () => {
+      const response = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', 'validToken');
+
+      expect(response.status).to.equal(200);
+    });
+
+    it('should return the user role', async () => {
+      const response = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', 'validToken');
+
+      expect(response.body).to.deep.equal({ role: 'admin' });
+    });
+  });
+
+  describe('when token is not sent', () => {
+    beforeEach(() => {
+      sinon.stub(JWT, 'decode').returns({ email: userMock.email, password: '' });
+      sinon.stub(User, 'findOne').resolves(userMock as User);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should return status code 400 with the error message', async () => {
+      const response = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', '');
+
+      expect(response.status).to.equal(400);
+      expect(response.body).to.deep.equal({ message: 'Invalid token' });
+    });
+  });
+
+  describe('when user does not exist', () => {
+    beforeEach(() => {
+      sinon.stub(JWT, 'decode').returns({ email: userMock.email, password: '' });
+      sinon.stub(User, 'findOne').resolves(null);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should return status code 404 with the error message', async () => {
+      const response = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', 'validToken');
+
+      expect(response.status).to.equal(404);
+      expect(response.body).to.deep.equal({ message: 'User not found' });
+    });
+  });
+});
