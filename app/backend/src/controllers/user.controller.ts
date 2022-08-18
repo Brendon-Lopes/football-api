f1ab71d451f8c09d40d430160974fc5d60d42335
Brendon <brendon.lopes.21@gmail.com>
2022-08-18 18:12:16 -0300

    feat: :sparkles: adds validate method to UserController class

--- app/backend/src/controllers/user.controller.ts
+++ app/backend/src/controllers/user.controller.ts
@@ -1,11 +1,19 @@
 import { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import statusCodes from 'http-status-codes';
 import UserService from '../services/user.service';
 
 export default class UserController {
   static async login(req: Request, res: Response) {
     const { email, password } = req.body;
-    const token = await UserService.login(email, password);
-    return res.status(StatusCodes.OK).json({ token });
+    const token = await UserService.login({ email, password });
+    return res.status(statusCodes.OK).json({ token });
+  }
+
+  static async validate(req: Request, res: Response) {
+    const { authorization: token } = req.headers;
+
+    const role = await UserService.validate(token);
+
+    return res.status(statusCodes.OK).json({ role });
   }
 }
