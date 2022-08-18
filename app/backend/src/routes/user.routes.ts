98fbb8a6b4fb4409114fc58f9c6517c7219fbab3
Brendon <brendon.lopes.21@gmail.com>
2022-08-18 18:12:53 -0300

    feat: :sparkles: adds route /login/validate

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import Validate from '../middlewares/loginValidation.middleware';
 import UserController from '../controllers/user.controller';
 
 const router = Router();
 
-router.post('/', UserController.login);
+router.post('/', Validate.login, UserController.login);
+router.get('/validate', UserController.validate);
 
 export default router;
