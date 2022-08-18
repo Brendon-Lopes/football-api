30d98fbdf82a542b1f5e2e44cf5d9816d32350f7
Brendon <brendon.lopes.21@gmail.com>
2022-08-18 18:11:46 -0300

    feat: :sparkles: adds validate method to UserService class

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -1,23 +1,42 @@
 import statusCodes from 'http-status-codes';
+import ILoginDTO from '../interfaces/ILoginDTO.interface';
 import CustomError from '../helpers/CustomError';
 import User from '../database/models/user';
 import PasswordHandler from '../helpers/PasswordHandler';
 import jwt from '../helpers/jwt';
 
 export default class UserService {
-  static async login(email: string, password: string): Promise<string> {
-    const user = await User.findOne({
-      where: {
-        email,
-      },
-    });
+  static async findOne(email: string) {
+    const user = await User.findOne({ where: { email } });
 
-    if (user === null) throw new CustomError(statusCodes.NOT_FOUND, 'Invalid fields');
+    if (user === null) {
+      throw new CustomError(statusCodes.UNAUTHORIZED, 'Incorrect email or password');
+    }
+
+    return user;
+  }
+
+  static async login({ email, password }: ILoginDTO): Promise<string> {
+    const user = await UserService.findOne(email);
 
     PasswordHandler.check(password, user.password);
 
     const token = jwt.sign({ email, password });
 
     return token;
+  }
+
+  static async validate(token: string | undefined) {
+    if (!token) throw new CustomError(statusCodes.BAD_REQUEST, 'Invalid token');
+
+    const { email } = jwt.decode(token);
+
+    const user = await User.findOne({ where: { email } });
+
+    if (user === null) {
+      throw new CustomError(statusCodes.NOT_FOUND, 'User not found');
+    }
+
+    return user.role;
   }
 }
