821e3aaf9e6c73d95d834b728c3c4f90c56eccc6
Brendon <brendon.lopes.21@gmail.com>
2022-08-19 17:45:24 -0300

    feat: :sparkles: adds route to get matches filtered by inProgress value (boolean)

--- app/backend/src/controllers/match.controller.ts
+++ app/backend/src/controllers/match.controller.ts
@@ -3,7 +3,19 @@ import statusCodes from 'http-status-codes';
 import MatchesService from '../services/match.service';
 
 export default class MatchesController {
-  static async getAll(_req: Request, res: Response) {
+  static async getAll(req: Request, res: Response) {
+    const { inProgress } = req.query;
+
+    if (inProgress !== null && inProgress === 'false') {
+      const matches = await MatchesService.getAllFinished();
+      return res.status(statusCodes.OK).json(matches);
+    }
+
+    if (inProgress) {
+      const matches = await MatchesService.getAllInProgress();
+      return res.status(statusCodes.OK).json(matches);
+    }
+
     const matches = await MatchesService.getAll();
     return res.status(statusCodes.OK).json(matches);
   }
--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -12,4 +12,28 @@ export default class MatchesService {
 
     return matches;
   }
+
+  static async getAllInProgress() {
+    const matches = await Match.findAll({
+      where: { inProgress: true },
+      include: [
+        { model: Team, as: 'teamHome', attributes: ['teamName'] },
+        { model: Team, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+
+    return matches;
+  }
+
+  static async getAllFinished() {
+    const matches = await Match.findAll({
+      where: { inProgress: false },
+      include: [
+        { model: Team, as: 'teamHome', attributes: ['teamName'] },
+        { model: Team, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+
+    return matches;
+  }
 }
