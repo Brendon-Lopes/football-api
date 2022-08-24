cd4782eae9fa38df58a830a9a094ce8bb8f6219e
Brendon <brendon.lopes.21@gmail.com>
2022-08-24 17:24:09 -0300

    test: :white_check_mark: adds tests to route PATCH /matches/:id

--- app/backend/src/tests/match.update.test.ts
+++ app/backend/src/tests/match.update.test.ts
@@ -11,10 +11,14 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const matchMock = {
+   id: 1, homeTeam: 1, awayTeam: 2, homeTeamGoals: 1, awayTeamGoals: 2, inProgress: true
+}
+
 describe('PATCH /matches/:id/finish', () => {
   describe('when it succeeds', () => {
     beforeEach(() => {
-      sinon.stub(Match, 'findByPk').resolves({ id: 1, homeTeam: 1, awayTeam: 2, homeTeamGoals: 1, awayTeamGoals: 2, inProgress: true } as Match);
+      sinon.stub(Match, 'findByPk').resolves(matchMock as Match);
       sinon.stub(Match, 'update').resolves([ 0 ] as any);
     });
 
@@ -25,9 +29,54 @@ describe('PATCH /matches/:id/finish', () => {
     it('should return status code 200', async () => {
       const response = await chai
         .request(app)
-        .patch('/matches/1/finish')
+        .patch('/matches/1/finish');
 
       expect(response.status).to.equal(StatusCodes.OK);
+    });
+  });
+
+  describe('when it fails', () => {
+    beforeEach(() => {
+      sinon.stub(Match, 'findByPk').resolves(null);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should return status code 404', async () => {
+      const response = await chai
+        .request(app)
+        .patch('/matches/1/finish');
+
+      expect(response.status).to.equal(StatusCodes.NOT_FOUND);
+    });
+  });
+});
+
+const matchUpdateSentDataMock = {
+  homeTeamGoals: 3,
+  awayTeamGoals: 1,
+};
+
+describe('PATCH /matches/:id', () => {
+  describe('when it succeeds', () => {
+    beforeEach(() => {
+      sinon.stub(Match, 'findByPk').resolves(matchMock as Match);
+      sinon.stub(Match, 'update');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('returns status code 200', async () => {
+      const response = await chai
+        .request(app)
+        .patch('/matches/1')
+        .send(matchUpdateSentDataMock);
+
+      expect(response.status).to.equal(200);
     });
   });
 });
